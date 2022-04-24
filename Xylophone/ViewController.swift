e7c9e2a157c09f1ef2cd6e6145212a4379728d58
Kim Jungmin <87885175+wondermini@users.noreply.github.com>
2022-04-24 22:28:17 +0900

    Xylophone App 만들기 완성

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -16,15 +16,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+ 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+
+       
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
     }
+    
 }
